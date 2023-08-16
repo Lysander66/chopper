a58af21a62758dc086a4d9298ff827ec998545f3
Lysander <mgician3@gmail.com>
2023-08-16 23:05:47 +0800

    1

--- diary/2023.go
+++ diary/2023.go
@@ -7,5 +7,14 @@ var (
 		{"2023-07-30", SideLateralRaise, 0, 30, ""},
 		{"2023-07-30", DumbbellRow, 0, 15, ""},
 		{"2023-07-30", Sport, 55, 0, ""},
+		{"2023-08-02", SideLateralRaise, 0, 100, ""},
+		{"2023-08-02", DumbbellRow, 0, 40, ""},
+		{"2023-08-12", DumbbellInclineBenchPress, 0, 58, "15,13,8,10,12"},
+		{"2023-08-12", DumbbellRow, 0, 20, ""},
+		{"2023-08-13", SideLateralRaise, 0, 83, "50,33"},
+		{"2023-08-13", PushUp, 0, 10, ""},
+		{"2023-08-15", SideLateralRaise, 0, 80, "50,30"},
+		{"2023-08-15", DumbbellRow, 0, 20, ""},
+		{"2023-08-16", PushUp, 0, 39, "10,10,9"},
 	}
 )
